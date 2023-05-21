6c23b70e07059499ce29d494f0d22b80fc88aaeb
Dominik Uher <uher.dom@gmail.com>
2023-05-21 21:41:38 +0200

    version 1.0

---
+++ StatisticMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StatisticMVC.Models;
+
+namespace StatisticMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            StatisticCalculator statisticCalculator = new StatisticCalculator();
+            return View(statisticCalculator);
+        }
+        [HttpPost]
+        public IActionResult Index(StatisticCalculator statisticCalculator)
+        {
+            if( ModelState.IsValid )
+            {
+                statisticCalculator.ConvertList(statisticCalculator.InputText);
+                statisticCalculator.GetMaxNumber();
+                statisticCalculator.GetMinNumber();
+                statisticCalculator.GetAvgNumber();
+                statisticCalculator.GetSortedNumbers();
+            }
+            return View(statisticCalculator);
+        }
+    }
+}
---
+++ StatisticMVC/Models/StatisticCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace StatisticMVC.Models
+{
+    public class StatisticCalculator
+    {
+        public string InputText { get; set; } = "";
+        public List<int> Numbers { get; private set; }
+        public int MaxNumber { get; private set; } = 0;
+        public int MinNumber { get; private set; } = 0;
+        public double AvgNumber { get; private set; }
+        public string SortedNumbers { get; set; } = "";
+
+        public StatisticCalculator() { 
+        Numbers = new List<int>();
+        }
+
+        public void GetMaxNumber()
+        {
+            MaxNumber = Numbers.Max();
+        }
+        public void GetMinNumber()
+        {
+            MinNumber = Numbers.Min();
+        }
+        public void GetAvgNumber()
+        {
+            AvgNumber = Numbers.Average();
+        }
+        public void GetSortedNumbers()
+        {
+            SortedNumbers = String.Join(",", Numbers.ToArray());
+        }
+
+        public void ConvertList(string inputText)
+        {
+            List<string> numbers = new List<string>();
+            numbers= inputText.Split(' ').ToList();
+            Regex regex = new Regex(@"^\d+");
+            foreach (string number in numbers)
+            {
+                
+                if (regex.IsMatch(number)) { 
+                Numbers.Add(Convert.ToInt32(number));
+                       }
+            }
+            
+            Numbers.Sort();
+        }
+    }
+}
